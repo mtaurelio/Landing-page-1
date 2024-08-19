7b0f69d22658706c11c64f3c72e2793338584fa6
mtaurelio <matheusaureliodeo4@gmail.com>
2024-08-19 10:32:29 -0300

    landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu");
+const Navmenu = document.querySelector(".nav-menu");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  Navmenu.classList.toggle("ativo");
+});
